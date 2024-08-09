705c2ea908387762c7e24541c08198a0547ebae0
NinjaFerozBR <gabrielcoosta10@hotmail.com>
2024-08-09 20:56:02 -0300

    Adicionar arquivos de projeto.

---
+++ AULA 3/Program.cs
@@ -0,0 +1,18 @@
+﻿double numero;
+int numero2;
+bool validador = true;
+while (validador == true)
+{
+    Console.Write("Digite um numero inteiro : ");
+    numero = Convert.ToDouble(Console.ReadLine());
+    numero2 = (int)numero;
+    if (numero2 != numero)
+    {
+        Console.WriteLine("Erro ! Não é um numero inteiro");
+    }
+    else
+    {
+        validador = false;
+    }
+}
+Console.Write("O numero é inteiro ");
---
+++ Atleta/Program.cs
@@ -0,0 +1,39 @@
+﻿// faça um prorama que o usuario digite quantos quilos um atleta levantou e pergunte seu nome
+// No final mostre quem ficou com ouro,prata e bronze
+// O programa perguntara o peso e o nome ate que alguem peça para o programa calcular o resultado
+
+double pesoOuro = 0;
+double peso;
+string nomeOuro = "";
+string nome;
+bool validador = false;
+string resposta;
+
+Console.WriteLine("Digite o nome do atleta");
+nome = Console.ReadLine();
+Console.WriteLine("Digite o peso levantado");
+peso = Convert.ToDouble(Console.ReadLine());
+
+while (validador == false)
+{
+    if (pesoOuro < peso)
+    {
+        pesoOuro = peso;
+        nomeOuro = nome;
+    }
+    Console.WriteLine("Gostaria de inserir dados de outro atleta? Sim ou não");
+    resposta = Console.ReadLine().ToLower();
+    if (resposta == "não" || resposta == "nao")
+    {
+        validador = true;
+    }
+    else
+    {
+        Console.WriteLine("Digite o nome do atleta");
+        nome = Console.ReadLine();
+        Console.WriteLine("Digite o peso levantado");
+        peso = Convert.ToDouble(Console.ReadLine());
+    }
+}
+Console.WriteLine("Atleta que recebeu ouro: " + nomeOuro);
+Console.WriteLine("Peso que ele levantou: " + pesoOuro + "kg");
